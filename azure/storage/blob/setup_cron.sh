a6980b8fae7ce2c8571e200d3af778b6af5c4d12
Jozef Rebjak <jozefrebjak@icloud.com>
2024-12-17 14:38:21 +0100

    fix: Update cron job in setup_cron.sh to source .env file before execution

--- azure/storage/blob/setup_cron.sh
+++ azure/storage/blob/setup_cron.sh
@@ -9,9 +9,10 @@ CRON_SCHEDULE=${CRON_SCHEDULE:-$DEFAULT_SCHEDULE}
 
 # Get the absolute path of the download.sh script
 SCRIPT_PATH=$(realpath download.sh)
+ENV_FILE=$(realpath .env)
 
 # Add the cron job with output redirection to a log file
-(crontab -l 2>/dev/null; echo "$CRON_SCHEDULE /bin/bash -c '$SCRIPT_PATH; logger -t download_script \"Download script ran at \$(date)\"' >> /var/log/download_script.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "$CRON_SCHEDULE /bin/bash -c 'source $ENV_FILE && $SCRIPT_PATH; logger -t download_script \"Download script ran at \$(date)\"' >> /var/log/download_script.log 2>&1") | crontab -
 
 echo "Cron job set to run the download script at the following schedule: $CRON_SCHEDULE"
 echo "Output will be logged to /var/log/download_script.log"

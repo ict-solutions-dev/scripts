1087178c0ecd3808965d00f49fc5daba4bd24199
Jozef Rebjak <jozefrebjak@icloud.com>
2024-12-17 14:53:49 +0100

    fix: Update cron job logging message in setup_cron.sh for clarity

--- azure/storage/blob/setup_cron.sh
+++ azure/storage/blob/setup_cron.sh
@@ -12,7 +12,7 @@ SCRIPT_PATH=$(realpath download.sh)
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
 # Add the cron job with logging to syslog
-(crontab -l 2>/dev/null; echo "$CRON_SCHEDULE cd $SCRIPT_DIR && /bin/bash download.sh && logger -t azure-blob-download \"Download script ran at \$(date)\"") | crontab -
+(crontab -l 2>/dev/null; echo "$CRON_SCHEDULE cd $SCRIPT_DIR && /bin/bash download.sh && logger -t azure-blob-download \"Azure Blob download script ran at \$(date)\"") | crontab -
 
 echo "Cron job set to run the download script at the following schedule: $CRON_SCHEDULE"
 echo "Output will be logged to syslog with the tag 'azure-blob-download'"

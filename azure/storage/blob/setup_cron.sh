c62c0ebf6ada5b3305274c8f812f6c1a496f622e
Jozef Rebjak <jozefrebjak@icloud.com>
2024-12-17 14:32:13 +0100

    fix: Update cron job in setup_cron.sh to log output to a file

--- azure/storage/blob/setup_cron.sh
+++ azure/storage/blob/setup_cron.sh
@@ -10,7 +10,8 @@ CRON_SCHEDULE=${CRON_SCHEDULE:-$DEFAULT_SCHEDULE}
 # Get the absolute path of the download.sh script
 SCRIPT_PATH=$(realpath download.sh)
 
-# Add the cron job
-(crontab -l 2>/dev/null; echo "$CRON_SCHEDULE /bin/bash -c '$SCRIPT_PATH; logger -t download_script \"Download script ran at \$(date)\"'") | crontab -
+# Add the cron job with output redirection to a log file
+(crontab -l 2>/dev/null; echo "$CRON_SCHEDULE /bin/bash -c '$SCRIPT_PATH; logger -t download_script \"Download script ran at \$(date)\"' >> /var/log/download_script.log 2>&1") | crontab -
 
 echo "Cron job set to run the download script at the following schedule: $CRON_SCHEDULE"
+echo "Output will be logged to /var/log/download_script.log"
